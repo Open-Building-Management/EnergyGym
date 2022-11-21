ae21dd209caa10c07cef8c41854c99cb6a086356
Alexandre CUER <alexandre.cuer@cerema.fr>
2022-11-21 11:12:33 +0100

    Update __init__.py

--- energy_gym/__init__.py
+++ energy_gym/__init__.py
@@ -1,4 +1,4 @@
-"""energygym module"""
+"""energy_gym module"""
 from .heatgym import Vacancy, Building
 from .evaluation_toolbox import Environnement, Evaluate
 from .tools import pick_name, get_feed, get_truth
