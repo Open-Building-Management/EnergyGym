838ef4230b78861a97a9a2baa73b250ed8aeb421
Alexandre CUER <alexandre.cuer@cerema.fr>
2023-01-27 18:03:48 +0100

    Update view_model.py

--- view_model.py
+++ view_model.py
@@ -1,6 +1,9 @@
+"""ouvre un réseau neurones et en affiche le détail
+notamment le nombre de neurones par couches
+"""
+import sys
 from energy_gym import pick_name
 from basicplay import load
-
 
 agent_path, saved = pick_name()
 if not saved :
