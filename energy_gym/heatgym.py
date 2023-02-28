f875b24f1f3e4e3d2a99417a7fbb3416c9392fcf
Alexandre CUER <alexandre.cuer@cerema.fr>
2023-02-28 11:12:23 +0100

    pdoc exclusion test

--- energy_gym/heatgym.py
+++ energy_gym/heatgym.py
@@ -9,6 +9,9 @@ import numpy as np
 
 from .planning import get_random_start, get_level_duration
 
+__pdoc__ = {}
+__pdoc__['gym.Env.action_space'] = False
+
 # modèle par défault de type R1C1 obtenues par EDW avec les données de Marc Bloch
 #MODELRC = {"R": 2.54061406e-04, "C": 9.01650468e+08}
 MODELRC = {"R": 5.94419964e-04, "C": 5.40132642e+07}
