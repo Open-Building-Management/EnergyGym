6d08448284727d21a9a52123a246167dd5936b1f
Alexjunk <alexandre.cuer@cerema.fr>
2023-03-25 19:15:16 +0100

    Update view_tensorboard_graph.py

--- view_tensorboard_graph.py
+++ view_tensorboard_graph.py
@@ -12,17 +12,12 @@ def trace(x):
     return primary_network(x)
 
 
-def graph(primary_network):
-    """generate the graph model"""
+if __name__ == "__main__":
+    primary_network = DQModel(50, 2)
+    primary_network.compile(optimizer=keras.optimizers.Adam(), loss='mse')
     logdir = "TensorBoard/graph"
     train_writer = tf.summary.create_file_writer(logdir)
     tf.summary.trace_on(graph=True, profiler=False)
     trace(tf.zeros((1,4)))
     with train_writer.as_default():
         tf.summary.trace_export(name="dueling_graph", step=0)
-
-
-if __name__ == "__main__":
-    primary_network = DQModel(50, 2)
-    primary_network.compile(optimizer=keras.optimizers.Adam(), loss='mse')
-    graph(primary_network)
