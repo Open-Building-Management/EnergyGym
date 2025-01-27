a2b8ee8ddca7ccdd61f7eac336d99badfb69ff47
Alexandre CUER <alexandrep.cuer@gmail.com>
2025-01-27 19:05:29 +0100

    Use tensorflow2.8.0 in devcontainer

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# setup for the devcontainer
+apt-get update
+apt-get install -y python3-tk
+python3 -m pip install matplotlib==3.1.2
+python3 -m pip install click
+python3 -m pip install gym
+python3 -m pip install PyFina
