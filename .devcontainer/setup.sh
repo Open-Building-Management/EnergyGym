2195c94a96297c02e7adb51b64a13f3230fd91d9
Alexandre CUER <alexandre.cuer@wanadoo.fr>
2025-01-29 11:53:40 +0100

    Add tf-keras to devcontainer
    should solve slow training pb du to to retracing
    https://github.com/Open-Building-Management/EnergyGym/issues/20
    
    code refactoring necessary :
    
    import tf_keras as keras

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,7 +2,8 @@
 # setup for the devcontainer
 apt-get update
 apt-get install -y python3-tk
-python3 -m pip install matplotlib==3.1.2
+python3 -m pip install tf-keras==$TF_VERSION
+python3 -m pip install matplotlib
 python3 -m pip install click
 python3 -m pip install gym
 python3 -m pip install PyFina
